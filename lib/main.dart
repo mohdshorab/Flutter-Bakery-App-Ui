54995f8988358d56bf6da79cb194a010b2953edb
mohdshorab <mohdshorab474@gmail.com>
2022-06-05 19:05:36 +0530

    added bakeryUI file

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/mainPage.dart';
+
+void main(){
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BakeryApp(),
+    )
+  );
+}
